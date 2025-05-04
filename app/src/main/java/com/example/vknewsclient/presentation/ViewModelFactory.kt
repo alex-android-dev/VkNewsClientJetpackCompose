0a22a46d4c4f1fb68e76bd1b80427e5a83788ecd
avlnkov <alex.lenkov@mail.ru>
2025-05-04 08:50:15 +0300

    фикс бага

--- app/src/main/java/com/example/vknewsclient/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/vknewsclient/presentation/ViewModelFactory.kt
@@ -7,7 +7,6 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 @Suppress("UNCHECKED_CAST")
-@ApplicationScope
 class ViewModelFactory @Inject constructor(
     private val viewModelProvider: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
@@ -15,4 +14,5 @@ class ViewModelFactory @Inject constructor(
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return viewModelProvider[modelClass]?.get() as T
     }
+
 }
